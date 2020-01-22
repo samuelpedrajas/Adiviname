aa6eb9784a985da7e344b0cf6958d2eca70c5fc1
Samuel P <samuel.p991@gmail.com>
2020-01-22 20:21:10 +0100

    Fixes #1

--- scripts/ConfigureGamePopup.gd
+++ scripts/ConfigureGamePopup.gd
@@ -29,3 +29,30 @@ func _on_PlayButton_pressed():
 	var saved_game = DB.insert_saved_game(n_teams, game_name)
 	Main.load_saved_game(saved_game)
 	Main.load_game(game_id)
+
+# copied code
+
+export(int) var LIMIT = 30
+var current_text = ''
+var cursor_line = 0
+var cursor_column = 0
+
+func _on_TextEdit_text_changed():
+	var text_edit = $Content/VBoxContainer/TextEdit
+	var new_text : String = text_edit.text
+	if new_text.length() > LIMIT:
+		text_edit.text = current_text
+		# when replacing the text, the cursor will get moved to the beginning of the
+		# text, so move it back to where it was
+		text_edit.cursor_set_line(cursor_line)
+		text_edit.cursor_set_column(cursor_column)
+
+	current_text = text_edit.text
+	# save current position of cursor for when we have reached the limit
+	cursor_line = text_edit.cursor_get_line()
+	cursor_column = text_edit.cursor_get_column()
+
+	if current_text == "":
+		$Content/PlayButton.set_disabled(true)
+	else:
+		$Content/PlayButton.set_disabled(false)
