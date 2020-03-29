ce6de12397b4c91fb41b5cf19c37806c58947f71
Samuel P <samuel.p991@gmail.com>
2020-03-29 20:11:54 +0200

    wip

--- scripts/PlayGamePopup.gd
+++ scripts/PlayGamePopup.gd
@@ -4,11 +4,12 @@ var new_game = false
 var game_list_item
 var saved_games
 
+
 func _ready():
 	saved_games = DB.get_saved_games_and_results()
 	$Content/SaveGameList.setup(saved_games)
 	$Content/CheckBox.set_pressed(Main.team_mode)
-	call_deferred("update_game_mode", Main.team_mode)
+	call_deferred("update_game_mode", Main.team_mode, false)
 
 func set_game_info(game_list_item):
 	self.game_list_item = game_list_item
@@ -17,21 +18,21 @@ func set_game_info(game_list_item):
 	$Content/Description.set_text(game_list_item.game_description)
 
 func _on_CheckBox_toggled(button_pressed):
-	update_game_mode(button_pressed)
+	update_game_mode(button_pressed, true)
 
-func update_game_mode(game_mode):
+func update_game_mode(game_mode, update_size=true):
 	if game_mode:
 		var selected_item = $Content/SaveGameList.selected_item
 		if selected_item.save_game_id != -1:
 			Main.load_saved_game(selected_item.save_game)
 			$Content/Turno.show()
 		$Content/SaveGameList.show()
-		_update_btn()
+		_update_btn(update_size)
 	else:
 		Main.solo_mode()
 		$Content/SaveGameList.hide()
 		$Content/Turno.hide()
-		_set_play_game_btn()
+		_set_play_game_btn(update_size)
 
 func _on_CloseButton_pressed():
 	Main.close_popups()
@@ -42,20 +43,44 @@ func _on_Button_pressed():
 	else:
 		Main.load_game(game_list_item.game_id)
 
-func _set_new_game_btn():
-	$Content/Button.set_text("Nueva Partida")
+func _set_new_game_btn(update_size=true):
+	# TODO
+	#$Content/Button.set_text("Nueva Partida")
+	var original_w_size = $Content.get_size()
+	if update_size:
+		var y_offset = $Content/SaveGameList.get_size().y
+
+		$Content.set_size(
+			original_w_size + Vector2(0, y_offset)
+		)
+		$Content/NinePatchRect.set_size(
+			original_w_size + Vector2(0, y_offset)
+		)
+		#$Content/Button.rect_position.y += y_offset
 	new_game = true
 
-func _set_play_game_btn():
-	$Content/Button.set_text("Jugar")
+func _set_play_game_btn(update_size=true):
+	# TODO
+	#$Content/Button.set_text("Jugar")
+	var original_w_size = $Content.get_size()
+	if update_size:
+		var y_offset = $Content/SaveGameList.get_size().y
+
+		$Content.set_size(
+			original_w_size - Vector2(0, y_offset)
+		)
+		$Content/NinePatchRect.set_size(
+			original_w_size - Vector2(0, y_offset)
+		)
+		#$Content/Button.rect_position.y -= y_offset
 	new_game = false
 
-func _update_btn():
+func _update_btn(update_size=true):
 	var item = $Content/SaveGameList.get_selected()
 	if item.save_game_id < 0:
-		_set_new_game_btn()
+		_set_new_game_btn(update_size)
 	else:
-		_set_play_game_btn()
+		_set_play_game_btn(update_size)
 
 func _on_SaveGameList_list_updated(item):
 	if item.save_game_id != -1:
