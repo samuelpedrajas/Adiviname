1a5ba52c053c0c68eb60e35dba9bec1a3193604c
Samuel P <samuel.p991@gmail.com>
2020-09-10 19:45:46 +0200

    results screen is multiscreen

--- scripts/GameListItem.gd
+++ scripts/GameListItem.gd
@@ -57,6 +57,6 @@ func setup(game_id, game_title, game_description, game_examples, game_icon_path,
 
 
 func _on_Button_pressed():
-	if not get_parent().get_parent().get_parent().swiping:
+	if not get_parent().get_parent().swiping:
 		Main.open_play_game_popup(self, false)
 		$Button/NotificationRect.hide()
